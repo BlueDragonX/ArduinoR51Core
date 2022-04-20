f6c854f4f5525c04dada4b18bfcdbf5992a73679
Ryan Bourgeois <bluedragonx@gmail.com>
2022-04-19 22:26:16 -0700

    Make Message copyable and assignable.

--- src/R51Core/Message.h
+++ src/R51Core/Message.h
@@ -33,16 +33,20 @@ class Message {
     public:
         // The message type.
         enum Type {
+            EMPTY,
             SYSTEM_EVENT,
             CAN_FRAME,
         };
 
+        // Default constructor. Sets type to EMPTY.
+        Message() : Message(EMPTY, nullptr) {}
+
         // Construct a message that references a system event.
-        Message(const SystemEvent& system_event) :
+        Message(SystemEvent& system_event) :
             type_(SYSTEM_EVENT), ref_(&system_event) {}
 
         // Construct a message that references a CAN frame.
-        Message(const Canny::Frame& can_frame) :
+        Message(Canny::Frame& can_frame) :
             type_(CAN_FRAME), ref_(&can_frame) {}
 
         // Return the type of the mesasge.
@@ -57,8 +61,10 @@ class Message {
         const Canny::Frame& can_frame() const { return *((Canny::Frame*)ref_); }
 
     private:
-        const Type type_;
-        const void* ref_;
+        Type type_;
+        void* ref_;
+
+        Message(Type type, void* ref) : type_(type), ref_(ref) {}
 };
 
 }  // namespace R51
